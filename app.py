4d51cb085049a04f2e56adfef7fd456611ecd954
alibaltschun <alibaltschun@gmail.com>
2019-10-10 21:10:08 -0700

    fixing get cewe unvoted and img rasio

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def get_cewe_unvoted(username):
     rows = [i[0] for i in rows]
     n = len([name for name in os.listdir("."+STATIC) if os.path.isfile("."+STATIC+"/"+name)])
     id_cewe = -1
-    for i in range(1,n+1):
+    for i in range(0,n+1):
         if i not in rows:
             id_cewe = i
             break
